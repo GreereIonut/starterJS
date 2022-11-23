09c633da8ed1b4332cd643eb1e1ef839c414845a
GreereIonut <65650700+GreereIonut@users.noreply.github.com>
2022-11-23 21:57:07 +0200

    getting far

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-let js='amazing';
-if (js=== 'amazing')
-alert("javascripting baby!!!");
+"use strict";
 
-console.log(30+21+231);
+
+
+/*let Dolphins;
+let Koalas;
+const calcAverage= (x,y,z)=>{
+    return (x+y+z)/3;
+};
+
+Dolphins=calcAverage(85,54,41);
+Koalas=calcAverage(23,34,27);
+
+function checkWinner(avgDolphins,avgKoalas){
+    if(avgDolphins>avgKoalas)
+    console.log(`Dolphins win with ${avgDolphins} vs ${avgKoalas}!`);
+    else
+    console.log(`Koalas win with ${avgKoalas} vs ${avgDolphins}!`);
+}
+checkWinner(Dolphins,Koalas);
+
+
+
+const calcTip=billValue=> billValue>=50 && billValue <=300 ? billValue*0.15 :billValue*0.2;
+
+const bills=[123,555,44];
+
+function showBill(bill){
+    console.log(bill);
+}
+
+const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+
+function addTipsBills(tips,bills)
+{
+    return tips+bills;
+}
+
+let total=[];
+console.log(total);
+for( let i=0;i<bills.length;++i)
+total.push(addTipsBills(tips[i],bills[i]));
+
+total.forEach(showBill);
+
+*/
